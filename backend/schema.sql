c7d18cae83481317a082436f3fda7d8bac19a9fd
falahkece123 <falahsatin1905@gmail.com>
2026-06-26 21:03:04 +0700

    Update schema.sql

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,1 +1,15 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    category TEXT NOT NULL,
+    status TEXT DEFAULT 'available'
+);
 
+-- Data Awal (Seed Data)
+INSERT INTO products (name, price, category) VALUES 
+('Kopi Susu Gula Aren', 22000, 'Coffee'),
+('Latte Jawain', 25000, 'Coffee'),
+('Matcha Latte', 26000, 'Non-Coffee'),
+('Croissant Almond', 28000, 'Pastry');
